65467dda2d5cbc0aaeea76077cd5231dd14adef2
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-04-09 23:57:05 +0300

    Remove error state

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,13 @@ import "./App.scss";
 export default function App() {
     const [tags, setTags] = useState<string[]>([]);
     const [labels, setLabels] = useState<string[]>([]);
-    const [error, setError] = useState<string | null>(null);
 
     async function handleText(text: string) {
         try {
             const response = await Api.markup.fetch(JSON.stringify({ text }));
             setTags(response.data?.tags || []);
             setLabels(response.data?.labels || []);
-            setError(response.error || null);
         } catch (e) {
-            setError("Ошибка сервера");
             console.error(e);
         }
     }
