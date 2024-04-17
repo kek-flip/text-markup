2b9a62d43ef3367f86261896b87f07adebc47485
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-04-17 23:00:36 +0300

    Add redirect to main from markup if text not provided

--- src/pages/MarkupPage/MarkupPage.tsx
+++ src/pages/MarkupPage/MarkupPage.tsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import MarkupViewer from "../../components/MarkupViewer/MarkupViewer";
 import TextViewer from "../../components/TextViewer/TextViewer";
 import { useMarkup } from "../../contexts/MarkupProvider/MarkupHooks";
 import Page from "../Page/Page";
+import { useEffect } from "react";
 
 import "./MarkupPage.scss";
 
 export default function MarkupPage() {
     const markup = useMarkup();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!markup.text) navigate("/")
+    });
 
     return (
-        <Page>
+        markup.text ? 
+        (<Page>
             <main className="markup-page">
                 <Link to="/" className="markup-page__button">
                     Markup new text
@@ -24,6 +31,7 @@ export default function MarkupPage() {
                     />
                 </div>
             </main>
-        </Page>
+        </Page>) :
+        (<div></div>)
     );
 }
